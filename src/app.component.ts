02c2280598ecce06d5a7085ce4665a8a66c07912
Michael J. Bennett <michael.bennett@rangle.io>
2016-01-05 11:54:58 -0500

    Completes Chapter 1

--- src/app.component.ts
+++ src/app.component.ts
@@ -2,9 +2,26 @@ import {Component} from 'angular2/core';
 
 @Component({
 	selector: 'app-root',
-	template: '<h1>Angular2 skeleton app</h1>'
+	template: `
+	<h1>{{title}}</h1>
+	<h2>{{programmer.name}} details</h2>
+	<div><label>id: </label>{{programmer.id}}</div>
+	<div>
+	  <label>name: </label>
+	  <input [(ngModel)]="programmer.name" placeholder="name"/>
+	</div>
+	`
 })
 
 export class AppComponent {
-	
+	public title = 'Tour of Programmers';
+	public programmer:Programmer = {
+		id: 1,
+		name: 'Rich'
+	};
+}
+
+interface Programmer {
+	id: number;
+	name: string;
 }
