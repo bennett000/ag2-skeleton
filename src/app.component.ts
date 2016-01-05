25063dde6db49905796f28fc9512009f3f413e7b
Michael J. Bennett <michael.bennett@rangle.io>
2016-01-05 12:56:28 -0500

    Completes Chapter 2

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,16 +1,87 @@
 import {Component} from 'angular2/core';
 
+var PROGRAMMERS: Programmer[] = [
+	{ "id": 11, name: "Alice" },
+	{ "id": 12, name: "Betty" },
+	{ "id": 13, name: "Cat" },
+	{ "id": 14, name: "Dora" },
+	{ "id": 15, name: "Emily" },
+	{ "id": 16, name: "Farah" },
+	{ "id": 17, name: "George" },
+	{ "id": 18, name: "Hannah" },
+	{ "id": 19, name: "Ingrid" },
+	{ "id": 20, name: "Joy" },
+];
+
 @Component({
 	selector: 'app-root',
 	template: `
 	<h1>{{title}}</h1>
-	<h2>{{programmer.name}} details</h2>
-	<div><label>id: </label>{{programmer.id}}</div>
-	<div>
-	  <label>name: </label>
-	  <input [(ngModel)]="programmer.name" placeholder="name"/>
+	<h2>Programmers</h2>
+	<ul class="programmers">
+	  <li *ngFor="#programmer of programmers"
+	      [class.selected]="programmer === selectedProgrammer"
+	      (click)="onSelect(programmer)">
+	    <span class="badge">{{programmer.id}}</span> {{ programmer.name }}
+	  </li>
+	</ul>
+	<div *ngIf="selectedProgrammer">
+	  <h2>{{selectedProgrammer.name}} details</h2>
+	  <div><label>id: </label>{{selectedProgrammer.id}}</div>
+	  <div>
+	    <label>name: </label>
+	    <input [(ngModel)]="selectedProgrammer.name" placeholder="name"/>
+	  </div>
 	</div>
-	`
+	`,
+	styles: [`
+	  .selected {
+	    background-color: #CFD8DC !important;
+	    color: white;
+	  }
+	  .programmers {
+	    margin: 0 0 2em 0;
+	    list-style-type: none;
+	    padding: 0;
+	    width: 10em;
+	  }
+	  .programmers li {
+	    cursor: pointer;
+	    position: relative;
+	    left: 0;
+	    background-color: #EEE;
+	    margin: 0.5em;
+	    padding: 0.3em 0em;
+	    height: 1.6em;
+	    border-radius: 4px;
+	  }
+	  .programmers li.selected:hover {
+	    color: blue;
+	  }
+	  .programmers li:hover {
+	    color: #607D8B;
+	    background-color: #EEE;
+	    left: 0.1em;
+	  }
+	  .programmers .text {
+	    position: relative;
+	    top: -3px;
+	  }
+	  .programmers .badge {
+	    display: inline-block;
+	    font-size: small;
+	    color: white;
+	    padding: 0.8em 0.7em 0em 0.7em;
+	    background-color: #607D8B;
+	    line-height: 1em;
+	    position: relative;
+	    left: -1px;
+	    top: -4px;
+	    height: 1.8em;
+	    margin-right: 0.8em;
+	    border-radius: 4px 0px 0px 4px;
+	  }
+	`]
 })
 
 export class AppComponent {
@@ -19,6 +90,11 @@ export class AppComponent {
 		id: 1,
 		name: 'Rich'
 	};
+	public selectedProgrammer: Programmer;
+	public programmers = PROGRAMMERS;
+	onSelect(programmer: Programmer) {
+		this.selectedProgrammer = programmer;
+	}
 }
 
 interface Programmer {
